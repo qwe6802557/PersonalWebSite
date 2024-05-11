2f76292d6edbe5b448a28e25f0710bc8b6eed678
Luoyang414 <425160813@qq.com>
2024-05-11 15:41:09 +0800

    初始化项目

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    plugins: [
+        ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }]
+    ],
+};
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button type="primary">点击我</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
